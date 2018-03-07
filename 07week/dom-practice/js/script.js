fdbe18edb62076d0057841f88d14aaaba25a1604
Dane Middleton <middletondane@gmail.com>
2018-03-06 18:12:16 -0600

    update index and javascript files for dom practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,63 +1,32 @@
 'use strict';
-//
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     // You code here
-// });
-//
-//
 
-window.onload = function(){
-  alert(setListMessage);
+window.onload = function() {
+// listItemCount()
 
+var newh2 = document.createElement('h2');
+newh2.innerHTML = "You have _ items in your shopping cart";
+
+document.getElementsByTagName('h1')[0].appendChild(newh2);
 
 
-  // var listCountMessage = setListMessage();
-  // alert(listCountMessage);
-  //
-  // createListCountHeaderElement();
-  // updateListCountHeaderMessage();
-  //
-  // createNewItemTextInput();
-  //
-  // addRemoveButtonstoExistingItems();
+function listItemCount(){
+  var list = document.getElementById('shopping-list');
+  var message = "There are "+ list.children.length + " items on the page.";
+  alert(message);
+
 }
 
-function setListMessage(){
-  let list = document.getElementById("shopping-list");
-  return "This page has "+ list.children.length + " items in the shopping cart."
+function addListItem() {
+    var newItem = document.createElement('li');
+    var input = document.getElementById('add item');
+    newItem.innerHTML = input.value;
+    input.value = '';
+    document.getElementById('shopping-list').appendChild(newItem);
 }
 
-function createListCountHeaderElement(message) {
-let listHeading= document.createElement("h2");
-document.getElementsByTagName("h1")[0].appendChild(listHeading)
-listHeading.setAttribute("id", "list-heading");
+function removeListItem() {
+  var item = document.getElementById('add item');
+  document.getElementById('shopping-list').removeChild(list.lastChild);
 }
 
-
-// function listCountMessage() {
-//   let listCount = document.getElementById("shopping-list");
-//   return list[0].children.length
-// }
-
-
-
-
-// var list = document.getElementsByTagName('ol');
-// var message = "This page has "+ list[0].children.length + " list items";
-// alert(message);
-//
-// var newh1 = document.createElement('h1');
-// newh1.innerHTML = "Manipulating the DOM!";
-// document.body.appendChild(newh1);
-//
-// var div =
-// document.getElementById('div');
-// var p3 =
-// document.getElementById('P3');
-// div.removeChild(p3);
-//
-// var list5 = document.createElement('li');
-// list5.innerHTML = "Fifth List Item";
-// list[0].appendChild(list5);
-//
-// document.body.classList.add('colors');
+}
